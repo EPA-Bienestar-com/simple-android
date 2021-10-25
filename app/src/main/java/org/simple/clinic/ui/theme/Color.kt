50b17c9caaad408e3090a3932ffb1c3c00c42047
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-25 07:17:18 +0530

    Add custom `Colors` extensions

---
+++ app/src/main/java/org/simple/clinic/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package org.simple.clinic.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+val Colors.colorToolbarPrimary
+  get() = Color(0xFF0C3966)
+
+val Colors.colorToolbarPrimaryVariant
+  get() = Color(0xFF0A2E52)
+
+val Colors.colorOnToolbarPrimary
+  get() = Color.White
+
+val Colors.onSurface34
+  get() = onSurface.copy(alpha = 0.34f)
