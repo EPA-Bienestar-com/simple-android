1790211ba447de3a65e078bc63765ddf5775b3f3
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-03-12 11:00:53 +0530

    Contribute `SetupActivityComponent` using `MergeSubcomponent` annotation
    This is similar to Dagger `Subcomponent` annotation, but this automatically merges the modules for a given scope.
    
    In later commits, we can remove included modules from the annotation and directly contribute from module. Anvil will take care of merging those with the sub component

--- app/src/main/java/org/simple/clinic/setup/SetupActivityComponent.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityComponent.kt
@@ -1,13 +1,20 @@
 package org.simple.clinic.setup
 
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.anvil.annotations.MergeSubcomponent
 import dagger.BindsInstance
 import dagger.Subcomponent
 import org.simple.clinic.navigation.v2.Router
 import org.simple.clinic.onboarding.OnboardingScreen
+import org.simple.clinic.scopes.OnboardingScope
+import org.simple.clinic.scopes.SingleIn
 import org.simple.clinic.splash.SplashScreen
 
-@Subcomponent(modules = [SetupActivityModule::class])
+@SingleIn(OnboardingScope::class)
+@MergeSubcomponent(
+    scope = OnboardingScope::class,
+    modules = [SetupActivityModule::class]
+)
 interface SetupActivityComponent : OnboardingScreen.Injector, SplashScreen.Injector {
 
   fun inject(target: SetupActivity)
